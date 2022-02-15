c756ef11fdbc86cfb40ea141bb0e86ccf12a5d2f
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-15 17:57:59 +0530

    Precedence

---
+++ Intro/04_Precedence.js
@@ -0,0 +1,9 @@
+// MDN - Mozilla Developer Network
+
+// typeof operator gives us the data type of operator.
+var result = true;
+console.log(typeof result, result);
+
+// Operators in JS follows a precedence order, Refer to the link provided,
+  
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
