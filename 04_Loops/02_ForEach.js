6fb3d5bfff35cdaff3cf50b6f219b0351a67dbe3
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-19 19:11:49 +0530

    For each

---
+++ 04_Loops/02_ForEach.js
@@ -0,0 +1,6 @@
+// For each is used to target each element of an array 
+// It uses a call back funtion to create method .
+
+const states = ["Bihar", "Rajasthan", "Up", 32, "asdkfh"];
+
+states.forEach((s) => (console.log(s)));
