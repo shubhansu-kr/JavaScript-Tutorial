a145b4c1be7fc7279050c65cbeeab56eb505144a
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-20 20:57:10 +0530

    Counting Followers

---
+++ 02_CounterProject.js
@@ -0,0 +1,29 @@
+// So the aim of this project is to add a counter on the website, 
+// which starts rolling from a number less than the target and 
+// stops at the target count in increasing order. 
+
+// This happens after every reload 
+
+// ./Projects/00_Project/
+
+// setTimeout(function, milliseconds) --> It sets the interval in which 
+// function is timed out (ie does not run).
+
+// setInterval(funtion, millisecond) --> It sets the interval in which 
+// the function is executed.
+
+// If we want to change the content of an element we have to first catch
+// that object, store it in a variable and then change the variable value 
+
+
+// Query selector return the tag 
+var count = document.querySelector(".counter") ; // class notation is used 
+// return the h1 tag 
+
+// count.innerText - gives the content 
+// we can assign any value to .counter text now
+
+// WE wil define a method to update the value of counter 
+// and then class that method using setInterval and setTimeout 
+
+
