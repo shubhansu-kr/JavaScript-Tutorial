8f30e806aa0f73777441d5f289366a069e26ab4d
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-20 19:58:41 +0530

    Dom

---
+++ 05_DOM/00_Dom.js
@@ -0,0 +1,5 @@
+// DOM - Document Object Model 
+
+// DOM is just a pictorial representation of the structure of document 
+// It's helpful in selecting the object or targetting an object 
+// with Js. 
