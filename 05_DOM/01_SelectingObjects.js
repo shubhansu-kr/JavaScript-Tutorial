d34ed3abc8f01d891e414d7419067fa9d5e17b12
shubhansu-kr <shubhansu2021@gmail.com>
2022-02-20 20:21:18 +0530

    Selecting Elements in Js

---
+++ 05_DOM/01_SelectingObjects.js
@@ -0,0 +1,17 @@
+// We can select the objects using the classes, tags, id, name etc
+
+// getting the elements using tag name 
+var title = document.getElementsByTagName("h1");
+
+// it returns an array, which contains a lot of properties and values
+
+// Accessing the elements of array returned by getelement method
+var title_content = document.getElementsByTagName("h1")[0].innerText ;
+
+
+// getting the elements using id 
+var title = document.getElementById("h1");
+
+// getting the elements using class 
+var title = document.getElementsByClassName("h1");
+
